6cc8d6e7e3993ec53f9c2e830f679e81df9db7a4
Samuel Viloria <samuelviloriadd@gmail.com>
2023-05-09 14:39:29 -0500

    agregamos mas iconos para diferentes climas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,32 @@
+import { useState,useEffect } from 'react'
 import clear from '../assets/imgs/1.svg'
+import clouds from '../assets/imgs/2.svg'
+import rain from '../assets/imgs/5.svg'
+import drizzle from '../assets/imgs/6.svg'
+import snow from '../assets/imgs/7.svg'
+import wind from '../assets/imgs/8.svg'
 
 export function Card({data}){
+
+    const [imgWeather, setImgWeather] = useState(null) 
+    let weather = data.weather[0].main
+
+    useEffect(() => {
+        if (weather === "Clear") {
+            setImgWeather(clear)
+        } else if (weather === "Clouds") {
+            setImgWeather(clouds)
+        } else if (weather === "Rain") {
+            setImgWeather(rain)
+        } else if (weather === "Drizzle") {
+            setImgWeather(drizzle)
+        } else if (weather === "Snow") {
+            setImgWeather(snow)
+        } else if (weather === "Wind") {
+            setImgWeather(wind)
+        }
+    }, []);
+    
     return(
         <div className="container-card">
                 <div className="info-temp">
@@ -11,9 +37,9 @@ export function Card({data}){
                     </div>
                     <div className="container-name">
                     <h2 className="name">{data.name}</h2>
-                    <h3 className="weather">{data.weather?.description}</h3>
+                    <h3 className="weather">{data.weather[0].description}</h3>
                     </div>
-                    <img src={clear} alt="" />
+                    <img src={imgWeather} alt="" />
                 </div>
         </div>
     )
